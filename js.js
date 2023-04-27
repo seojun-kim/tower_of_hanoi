34e93d5c8dbca31d43b4a616b850c4945c036fe0
seojun-kim <seojun1208@gmail.com>
2023-04-27 10:11:55 +0900

    first commit

---
+++ js.js
@@ -0,0 +1,5 @@
+const item = document.querySelector(".item");
+
+item.addEventListener("drag", (e) => {
+    console("드래그 시작 이벤트");
+})
---
+++ timer.js
@@ -0,0 +1,40 @@
+const timer = document.querySelector('.js-timer');
+// const stopBtn = document.querySelector('.js-timer_stopBtn');
+// const startBtn = document.querySelector('.js-timer_startBtn');
+// const resetBtn = document.querySelector('.js-timer_resetBtn');
+
+let TIME = 0;
+let cron;
+
+function startButton() {
+    resetButton();
+    updateTimer();
+    stopButton();
+    start = true;
+    cron = setInterval(updateTimer, 1000);
+    timer.classList.add('start');
+    reloadF();
+}
+
+function stopButton() {
+    clearInterval(cron);
+    timer.classList.remove('start');
+}
+
+function resetButton() {
+    timer.innerText = '00:00';
+    stopButton();
+    timer.classList.remove('start');
+    return (TIME = 0);
+}
+
+function updateTimer() {
+    const checkMinutes = Math.floor(TIME / 60);
+    const seconds = TIME % 60;
+    const minutes = checkMinutes % 60;
+
+    timer.innerText = `${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds}`;
+    TIME++;
+    // console.log(TIME - 1);
+}
